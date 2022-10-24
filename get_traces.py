4b5c721d6d8c6591c795d09f215b916a5ebce3e1
Aiden Chang <62921661+aiden200@users.noreply.github.com>
2022-10-24 00:07:49 -0500

    Automated program running

--- get_traces.py
+++ get_traces.py
@@ -23,6 +23,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 from scapy.all import *
 
+import subprocess
 
 
 
@@ -33,25 +34,35 @@ def main():
     # driver = webdriver.Chrome(chromedriver, chrome_options=options)
 
     browser = webdriver.Chrome()
-    print("Opening browser")
-    browser.get('https://open.spotify.com/collection/tracks')
-    print("Opened!")
+    # print("Opening browser")
+    browser.get('https://google.com')
+    # print("Opened!")
     # driver.find_element_by_css_selector('button.Button-qlcn5g-0.kgFBvD').click() #playing by clicking the play button
-    print("sniffing")
-    t = AsyncSniffer()#iface="eth0", offline="file.pcap") # not sure 
-    t.start()
-    time.sleep(3)
-    results = t.stop()
-    print(results)
+    capture = sniff(count=500)
+    # print("sniffing")
+    # t = AsyncSniffer()#iface="eth0", offline="file.pcap") # not sure 
+    # t.start()
+    # time.sleep(3)
+    # results = t.stop()
+    # print(results)
+    # capture.summary()
     # driver.find_element_by_css_selector('button.Button-qlcn5g-0.kgFBvD').click() #pausing by clicking the play button
-
-
+    i = 1
+    wrpcap(f"traces/google/google_trace{i}.pcap", capture)
+    browser.quit()
+    try:
+        with open(f'csv_files/google/google_trace{i}.csv','w') as f:
+            subprocess.run(f"tshark -r traces/google/google_trace{i}.pcap -T fields\
+            -e frame.number -e ip.src -e ip.dst \
+            -E header=y -E separator=/t".split(), stdout =f)
+    except Exception as e:
+        print(f"Iteration: {i}\n error: {e}")
+    #had to make link to tshark ln -s /Applications/Wireshark.app/Contents/MacOS/tshark /usr/local/bin/tshark
     # assert 'Yahoo' in browser.title
 
     # elem = browser.find_element(By.NAME, 'p')  # Find the search box
     # elem.send_keys('seleniumhq' + Keys.RETURN)
 
-    browser.quit()
 
 
 
