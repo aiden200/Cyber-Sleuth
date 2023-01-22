4a2107dd940b0f1ee86a3c0e664eb8c275d9effe
jonesj2 <63415841+jonesj2@users.noreply.github.com>
2023-01-22 17:30:21 -0600

    Update interface.py
    Moved a few buttons around and changed variable names

--- interface.py
+++ interface.py
@@ -10,6 +10,8 @@ WARNING:
 
 import tkinter as tk                # python 3
 from tkinter import font as tkfont  # python 3
+from tkinter.filedialog import askopenfile # python 3
+from scapy.all import *
 
 
 class SampleApp(tk.Tk):
@@ -28,18 +30,34 @@ class SampleApp(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+
+
         self.frames = {}
-        for F in (StartPage, PageOne, PageTwo):
+        for F in (StartPage, InstructionsPage, BackgroundPage, ProfilePage, AboutPage):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
-
             # put all of the pages in the same location;
             # the one on the top of the stacking order
             # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
-
+        
         self.show_frame("StartPage")
+
+#seperate frame setup for UploadTracePage to include
+#function for openening pcap files
+        for F in (StartPage, UploadTracePage):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+        
+        self.show_frame("StartPage")
+            
+
 
     def show_frame(self, page_name):
         '''Show a frame for the given page name'''
@@ -60,49 +78,99 @@ class StartPage(tk.Frame):
         sub_label.pack(side="top", fill="x", pady=10)
 
         button1 = tk.Button(self, text="Instructions",
-                            command=lambda: controller.show_frame("PageOne"))
+                            command=lambda: controller.show_frame("InstructionsPage"))
         button2 = tk.Button(self, text="Build Background Profile",
-                            command=lambda: controller.show_frame("PageTwo"))
+                            command=lambda: controller.show_frame("BackgroundPage"))
         button3 = tk.Button(self, text="Build Profile for a Website",
-                            command=lambda: controller.show_frame("PageTwo"))
+                            command=lambda: controller.show_frame("ProfilePage"))
         button4 = tk.Button(self, text="Upload Your Own Trace",
-                            command=lambda: controller.show_frame("PageTwo"))
+                            command=lambda: controller.show_frame("UploadTracePage"))
+        button5 = tk.Button(self, text="About", highlightbackground='grey', height= 5, width=10,
+                            command=lambda: controller.show_frame("AboutPage"))
+        button6 = tk.Button(self, text="Quit", highlightbackground='grey', height= 5, width=10, 
+                            command=controller.destroy)
         # button5 = tk.Button(self, text="Exit", command=self.destroy)
         button1.pack()
         button2.pack()
         button3.pack()
         button4.pack()
-        # button5.pack(side="bottom", pady=10) . 
+        button5.pack(anchor="s", side="right")
+        button6.pack(anchor="s", side="left")
+        
 
 
-class PageOne(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-
-        label = tk.Label(self, text="This is page 1", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
-
-
-class PageTwo(tk.Frame):
+class InstructionsPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="This is page 2", font=controller.title_font)
+        label = tk.Label(self, text="Instructions", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
+        button = tk.Button(self, text="Back to Start Page",
+            highlightbackground='grey', height= 5, width=12,
+            command=lambda: controller.show_frame("StartPage"))
+        button.pack(anchor="s", side="left")
+
+
+class BackgroundPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Tracing Computer Background", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Back to Start Page",
+            highlightbackground='grey', height= 5, width=12,
+            command=lambda: controller.show_frame("StartPage"))
+        button.pack(anchor="s", side="left")
+
+
+class ProfilePage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Building a Web Profile", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Back to Start Page",
+            highlightbackground='grey', height= 5, width=12,
+            command=lambda: controller.show_frame("StartPage"))
+        button.pack(anchor="s", side="left")
+
+
+class UploadTracePage(tk.Frame):
+  
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Upload Trace Here!", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        upload_instructions = tk.Button(self, text="Choose a File...", command=lambda: controller.show_frame("StartPage"))
+        upload_instructions.pack()
+        button = tk.Button(self, text="Back to Start Page",
+            highlightbackground='grey', height= 5, width=12,
+            command=lambda: controller.show_frame("StartPage"))
+        button.pack(anchor="s", side="left")
+
+
+
+class AboutPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="About This Project", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Back to Start Page",
+            highlightbackground='grey', height= 5, width=12,
+            command=lambda: controller.show_frame("StartPage"))
+        button.pack(anchor="s", side="left")
+
 
 
 if __name__ == "__main__":
     app = SampleApp()
-    app.geometry("800x600")
+    app.geometry("800x500")
     app.mainloop()
 
 '''
